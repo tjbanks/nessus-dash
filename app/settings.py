df777e2f03147695b100b48bfffa845ae894fdfc
Tyler Banks <tbanks@mail.missouri.edu>
2020-04-20 06:56:40 +0000

    settings changed

--- app/settings.py
+++ app/settings.py
@@ -35,14 +35,14 @@ CACHE_URL = False
 
 # Flask-User settings
 USER_APP_NAME = APP_NAME
-USER_ENABLE_CHANGE_PASSWORD = True  # Allow users to change their password
+USER_ENABLE_CHANGE_PASSWORD = False  # Allow users to change their password
 USER_ENABLE_CHANGE_USERNAME = False  # Allow users to change their username
-USER_ENABLE_CONFIRM_EMAIL = True  # Force users to confirm their email
-USER_ENABLE_FORGOT_PASSWORD = True  # Allow users to reset their passwords
-USER_ENABLE_EMAIL = True  # Register with Email
-USER_ENABLE_REGISTRATION = True  # Allow new users to register
-USER_REQUIRE_RETYPE_PASSWORD = True  # Prompt for `retype password` in:
-USER_ENABLE_USERNAME = False  # Register and Login with username
+USER_ENABLE_CONFIRM_EMAIL = False  # Force users to confirm their email
+USER_ENABLE_FORGOT_PASSWORD = False  # Allow users to reset their passwords
+USER_ENABLE_EMAIL = False  # Register with Email
+USER_ENABLE_REGISTRATION = False  # Allow new users to register
+USER_REQUIRE_RETYPE_PASSWORD = False  # Prompt for `retype password` in:
+USER_ENABLE_USERNAME = True  # Register and Login with username
 USER_AFTER_LOGIN_ENDPOINT = 'main.member_page'
 USER_AFTER_LOGOUT_ENDPOINT = 'main.member_page'
 USER_ALLOW_LOGIN_WITHOUT_CONFIRMED_EMAIL = False
